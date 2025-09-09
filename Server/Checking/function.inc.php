a5a27335ac6248f88edd652982263fb9ee6e6641
uvindux <uvindumanohara@icloud.com>
2025-09-09 10:40:39 +0530

    Add functions

---
+++ Server/Checking/function.inc.php
@@ -0,0 +1,23 @@
+<?php
+
+function emptyInputSignup($name,$email,$psw){
+          $result;
+          
+          if (empty($name) || empty ($email) ||empty ($psw) ){
+                    $result=true;
+          }
+          else
+          {
+                    $result=false;
+          }
+
+
+
+
+
+
+
+
+}
+
+?>
